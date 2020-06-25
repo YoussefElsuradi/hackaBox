f22c4ebe0c0010c384ee64d278baad2a4e88e43e
StrongestSwimmer <youssefelsuradi@gmail.com>
2020-06-25 13:43:32 -0400

    first commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from datetime import datetime
 
 
 # -- Initialization section --
@@ -13,13 +14,33 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', time = datetime.now())
 
-@app.route('/password', methods = ["GET", "POST"])
+@app.route('/password5', methods = ["GET", "POST"])
 def password():
     password1 = request.form["password"]
     print(password1)
     if password1 == "p@ssword123":
-        return render_template('results.html')
+        return render_template('results.html', time = datetime.now())
     else:
-        return render_template('index.html')
+        return render_template('index.html', time = datetime.now())
+
+@app.route('/index2')
+def index2():
+    return render_template('index2.html', time = datetime.now())
+
+@app.route('/password2', methods = ["GET", "POST"])
+def password2():
+    password3 = request.form["password"]
+    print(password3)
+    if password3 == "123456789":
+        return render_template('results2.html', time = datetime.now())
+    else:
+        return render_template('index2.html', time = datetime.now())
+
+@app.route('/password')
+def password5():
+    return render_template('password.html', time = datetime.now())
+
+
+
